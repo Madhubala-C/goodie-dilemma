1411f9591951595aa54ebbf8b7649a02c922ed72
Madhubala S C <madhubalachalamarad@gmail.com>
2022-10-03 16:05:24 +0530

    first commit

---
+++ Item.java
@@ -0,0 +1,13 @@
+class Item {
+    String name;
+    int price;
+  
+    public Item(String name, int price) {
+      this.name = name;
+      this.price = price;
+    }
+  
+    public String toString() { 
+        return this.name + ": " + this.price;
+    }
+  }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+c:\\Output\\output.txt
